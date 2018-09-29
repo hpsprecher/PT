dcadcf10a317621f38867809439e4fd96d1238a8
hpsprecher <Hanspeter.Sprecher@gmail.com>
2018-09-29 22:18:33 +0200

    Horwitz estimator shortend to calculate s from c direct without calculating c first

---
+++ horwitz_short.R
@@ -0,0 +1,14 @@
+horwitz_short<- function(x, na.rm=TRUE) {
+  library(metRology)
+  if(na.rm==TRUE)
+    x<- na.omit(x)
+  c<- x
+  
+  if (c<1.2*10^-7)
+    s<- 0.22*c
+  if(1.2*10^-7<=c & c<=0.138)
+    s<- 0.02*c^0.8495
+  if (c>0.138)
+    s<- 0.01*c^0.5
+  return(list(s= s))
+}
